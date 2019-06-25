5619c0edac7539475ba4346ce713b64205494f4e
Anupam Kaul <anupam.kaul@yahoo.com>
2019-06-25 15:03:47 -0700

    Change grasp object color to red
    Gazebo is affected but rviz is not. why?
    Also make xterm windows persistent as I wanna debug the errors.
    (start script kills all xterms upon enter .. launch.sh does not)

--- optional/kuka_challenge/workspace/catkin_ws/src/rll_planning_project/scripts/start_project.sh
+++ optional/kuka_challenge/workspace/catkin_ws/src/rll_planning_project/scripts/start_project.sh
@@ -4,13 +4,13 @@
 cd $(pwd)/../../.. ; catkin_make
 
 # Launch the nodes
-xterm  -e "source devel/setup.bash; roslaunch rll_planning_project moveit_planning_execution.launch" &
+xterm  -hold -e "source devel/setup.bash; roslaunch rll_planning_project moveit_planning_execution.launch" &
 sleep 10
-xterm  -e "source devel/setup.bash; roslaunch rll_planning_project planning_iface.launch" &
+xterm  -hold -e "source devel/setup.bash; roslaunch rll_planning_project planning_iface.launch" &
 sleep 4	
-xterm  -e "source devel/setup.bash; roslaunch rll_planning_project path_planner.launch" &
+xterm  -hold -e "source devel/setup.bash; roslaunch rll_planning_project path_planner.launch" &
 sleep 4
-xterm  -e "source devel/setup.bash; roslaunch rll_project_runner run_project.launch" &
+xterm  -hold -e "source devel/setup.bash; roslaunch rll_project_runner run_project.launch" &
 
 # Termiante the project
 echo "Press Enter key to terminate the project"
